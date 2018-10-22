533b608fcb64fea7a49ec9e4de55377fbc8b8969
Andi Hakim <andi.hakim.arif@gmail.com>
2018-10-22 16:46:20 +0700

    BLOG: views, urls

--- blog_project/blog/forms.py
+++ blog_project/blog/forms.py
@@ -14,7 +14,7 @@ class PostForm(forms.ModelForm):
 
 class CommentForm(forms.ModelForm):
     class Meta():
-        model Comment
+        model = Comment
         fields = ('author', 'text')
 
         widgets = {
--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -5,4 +5,12 @@ urlpatterns = [
     url('', views.PostListView.as_view(), name='post_list'),
     url('about/', views.AboutView.as_view(), name='about'),
     url('post/<int:pk>', views.PostDetailView.as_view(), name='post_detail'),
+    url('post/new/', views.CreatePostView.as_view(), name='post_new'),
+    url('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
+    url('post/<int:pk>/remove/', views.PostDeleteView.as_view(), name='post_remove'),
+    url('drafts/', views.DraftListView.as_view(), name='post_draft_list'),
+    url('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    url('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    url('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
+    url('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
 ]
